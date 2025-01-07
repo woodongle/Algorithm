6c3571fefb8e424ecf2186ccb7e1c0584c350a1e
woodongle <112540904+woodongle@users.noreply.github.com>
2025-01-07 21:21:30 +0900

    [Bronze III] Title: 최소, 최대, Time: 456 ms, Memory: 88240 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -1,23 +1,29 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int N = Integer.parseInt(br.readLine());
-		int[] arr = new int[N];
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < arr.length; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		Arrays.sort(arr);
-		
-		System.out.println(arr[0] + " " + arr[N - 1]);	
-	}
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int max = -1_000_000;
+        int min = 1_000_000;
+        int count = Integer.parseInt(br.readLine());
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < count; i++) {
+            int number = Integer.parseInt(st.nextToken());
+
+            if (max < number) {
+                max = number;
+            }
+
+            if (min > number) {
+                min = number;
+            }
+        }
+
+        System.out.println(min + " " + max);
+    }
 }
