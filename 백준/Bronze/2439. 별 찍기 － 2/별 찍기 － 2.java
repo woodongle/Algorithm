833e428a5b33e051a64821c22a5d2ce3d7af06c9
woodongle <112540904+woodongle@users.noreply.github.com>
2025-01-07 19:43:05 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 108 ms, Memory: 14264 KB -BaekjoonHub

--- 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -5,19 +5,20 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+        StringBuilder sb = new StringBuilder();
         int starCount = Integer.parseInt(br.readLine());
 
         for (int i = 1; i <= starCount; i++) {
             for (int j = starCount; j > i; j--) {
-                System.out.print(" ");
+                sb.append(" ");
             }
 
             for (int j = 0; j < i; j++) {
-                System.out.print("*");
+                sb.append("*");
             }
 
-            System.out.println();
+            sb.append("\n");
         }
+        System.out.println(sb);
     }
 }
