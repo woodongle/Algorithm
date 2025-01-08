4defffccbb5c4a950f6f151f9a43669da4e0e5ea
woodongle <112540904+woodongle@users.noreply.github.com>
2025-01-08 20:20:46 +0900

    [Bronze II] Title: 문자열 반복, Time: 104 ms, Memory: 14316 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.java
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.java
@@ -5,21 +5,23 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb;
         StringTokenizer st;
 
         int testCaseCount = Integer.parseInt(br.readLine());
 
         for (int i = 0; i < testCaseCount; i++) {
+            sb = new StringBuilder();
             st = new StringTokenizer(br.readLine());
 
             int R = Integer.parseInt(st.nextToken());
             String[] S = st.nextToken().split("");
 
             for (String s : S) {
-                System.out.print(s.repeat(R));
+                sb.append(s.repeat(R));
             }
-            
-            System.out.println();
+
+            System.out.println(sb);
         }
     }
 }
