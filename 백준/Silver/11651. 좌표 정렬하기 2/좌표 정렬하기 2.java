98f12e10a1258af0ed2af38df2c908aeef12276d
woodongle <112540904+woodongle@users.noreply.github.com>
2025-01-27 13:00:41 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 704 ms, Memory: 51240 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int coordinateCount = Integer.parseInt(br.readLine());
+        List<Coordinate> coordinates = new ArrayList<>();
+
+        StringTokenizer st;
+        for (int i = 0; i < coordinateCount; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            coordinates.add(new Coordinate(x, y));
+        }
+
+        coordinates.sort(new Comparator<Coordinate>() {
+            @Override
+            public int compare(Coordinate c1, Coordinate c2) {
+                if (c1.y_coordinate == c2.y_coordinate) {
+                    return c1.x_coordinate - c2.x_coordinate;
+                } else {
+                    return c1.y_coordinate - c2.y_coordinate;
+                }
+            }
+        });
+
+        StringBuilder sb = new StringBuilder();
+        for (Coordinate coordinate : coordinates) {
+            sb.append(coordinate.x_coordinate).append(" ").append(coordinate.y_coordinate).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+
+    static class Coordinate {
+        int x_coordinate;
+        int y_coordinate;
+
+        public Coordinate(int x_coordinate, int y_coordinate) {
+            this.x_coordinate = x_coordinate;
+            this.y_coordinate = y_coordinate;
+        }
+
+        @Override
+        public String toString() {
+            return x_coordinate + " " + y_coordinate;
+        }
+    }
+}
