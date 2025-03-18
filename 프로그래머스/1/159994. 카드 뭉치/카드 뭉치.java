11649a8e63e208ba5fc601736b7e96b40a1c4abf
woodongle <112540904+woodongle@users.noreply.github.com>
2025-03-18 20:20:06 +0900

    [level 1] Title: 카드 뭉치, Time: 1.40 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -2,44 +2,19 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] cards1, String[] cards2, String[] goal) {
-        ArrayDeque<String> cardQueue1 = createQueue(cards1);
-        ArrayDeque<String> cardQueue2 = createQueue(cards2);
-        ArrayDeque<String> goalQueue = createQueue(goal);
+        ArrayDeque<String> deck1 = new ArrayDeque(Arrays.asList(cards1));
+        ArrayDeque<String> deck2 = new ArrayDeque(Arrays.asList(cards2));
         
-        while (!goalQueue.isEmpty()) {
-            String answerCard = goalQueue.poll();
-            
-            if (!cardQueue1.isEmpty()) {
-                String card1 = cardQueue1.poll();
-                
-                if (answerCard.equals(card1)) {
-                    continue;
-                } else {
-                    cardQueue1.addFirst(card1);
-                }
-            }
-            
-            String card2 = "";
-            if (!cardQueue2.isEmpty()) {
-                card2 = cardQueue2.poll();
-            }
-            
-            if (answerCard.equals(card2)) {
-                continue;
+        for (String target : goal) {
+            if (!deck1.isEmpty() && deck1.peek().equals(target)) {
+                deck1.poll();
+            } else if (!deck2.isEmpty() && deck2.peek().equals(target)) {
+                deck2.poll();
             } else {
                 return "No";
             }
         }
         
         return "Yes";
-    }
-    
-    public ArrayDeque<String> createQueue(String[] arr) {
-        ArrayDeque<String> queue = new ArrayDeque<>();
-        for (int i = 0; i < arr.length; i++) {
-            queue.add(arr[i]);
-        }
-        
-        return queue;
     }
 }
