f0c301de0c4793dafc23499d03ebaf73e51663ce
woodongle <112540904+woodongle@users.noreply.github.com>
2025-04-25 15:41:56 +0900

    [D1] Title: 대각선 출력하기, Time: 79 ms, Memory: 24,576 KB -BaekjoonHub

---
+++ SWEA/D1/2027. 대각선 출력하기/대각선 출력하기.java
@@ -0,0 +1,21 @@
+class Solution {
+
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (i == j) {
+                    sb.append("#");
+                    continue;
+                }
+
+                sb.append("+");
+            }
+
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
