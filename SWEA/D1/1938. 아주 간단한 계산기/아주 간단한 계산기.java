f6ebee6d5d94a0a065aafc71cf4e255c1f74db15
woodongle <112540904+woodongle@users.noreply.github.com>
2025-04-25 14:44:13 +0900

    [D1] Title: 아주 간단한 계산기, Time: 77 ms, Memory: 25,216 KB -BaekjoonHub

---
+++ SWEA/D1/1938. 아주 간단한 계산기/아주 간단한 계산기.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int num1 = Integer.parseInt(st.nextToken());
+        int num2 = Integer.parseInt(st.nextToken());
+
+        System.out.println(num1 + num2);
+        System.out.println(num1 - num2);
+        System.out.println(num1 * num2);
+        System.out.println(num1 / num2);
+    }
+}
