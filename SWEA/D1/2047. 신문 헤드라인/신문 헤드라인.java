d0d3ea7a354a30cbcd7562d5ec30fad1c66b0353
woodongle <112540904+woodongle@users.noreply.github.com>
2025-04-25 14:13:12 +0900

    [D1] Title: 신문 헤드라인, Time: 74 ms, Memory: 25,472 KB -BaekjoonHub

---
+++ SWEA/D1/2047. 신문 헤드라인/신문 헤드라인.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+class Solution {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String T = br.readLine();
+        System.out.println(T.toUpperCase());
+    }
+}
