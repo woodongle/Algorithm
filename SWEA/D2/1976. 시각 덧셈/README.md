4d860df7cb9924775346dcd2b970a4fc68b2f387
woodongle <112540904+woodongle@users.noreply.github.com>
2025-05-02 17:04:28 +0900

    [D2] Title: 시각 덧셈, Time: 85 ms, Memory: 25,856 KB -BaekjoonHub

---
+++ SWEA/D2/1976. 시각 덧셈/README.md
@@ -0,0 +1,15 @@
+# [D2] 시각 덧셈 - 1976 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5PttaaAZIDFAUq) 
+
+### 성능 요약
+
+메모리: 25,856 KB, 시간: 85 ms, 코드길이: 938 Bytes
+
+### 제출 일자
+
+2025-05-02 17:04
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D2/1976. 시각 덧셈/시각 덧셈.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		int T = Integer.parseInt(br.readLine());
+
+		for (int testCase = 1; testCase <= T; testCase++) {
+			sb.append("#").append(testCase).append(" ");
+			
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int t1 = Integer.parseInt(st.nextToken());
+			int m1 = Integer.parseInt(st.nextToken());
+			int t2 = Integer.parseInt(st.nextToken());
+			int m2 = Integer.parseInt(st.nextToken());
+			int tSum = 0;
+			int mSum = 0;
+			
+			tSum = t1 + t2;
+			mSum = m1 + m2;
+			
+			if (mSum >= 60) {
+				mSum -= 60;
+				tSum += 1;
+			}
+			
+			if (tSum < 24) {
+				tSum %= 12;
+			} else {
+				tSum /= 2;
+			}
+			
+			sb.append(tSum).append(" ").append(mSum).append("\n");
+		}
+
+		System.out.println(sb);
+	}
+}
