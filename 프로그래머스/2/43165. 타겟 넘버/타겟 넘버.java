edc99e1866db704083f17b05aa48e9be4651ffe4
woodongle <112540904+woodongle@users.noreply.github.com>
2025-05-08 21:30:05 +0900

    [level 2] Title: 타겟 넘버, Time: 8.87 ms, Memory: 91.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -0,0 +1,29 @@
+class Solution {
+    
+    int answer;
+    int[] numbers;
+    int target;
+    
+    void dfs(int index, int sum) {
+        if (index == numbers.length) {
+            if (sum == target) {
+                answer++;
+            }
+            
+            return;
+        }
+        
+        dfs(index + 1, sum + numbers[index]);
+        dfs(index + 1, sum - numbers[index]);
+    }
+    
+    public int solution(int[] numbers, int target) {
+        answer = 0;
+        this.numbers = numbers;
+        this.target = target;
+        
+        dfs(0, 0);
+        
+        return answer;
+    }
+}
