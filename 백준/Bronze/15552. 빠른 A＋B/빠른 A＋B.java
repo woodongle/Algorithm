304460e06d6bf1e74f3e647b138e50d280406304
woodongle <112540904+woodongle@users.noreply.github.com>
2025-05-23 13:12:00 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 820 ms, Memory: 220684 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		int T = Integer.parseInt(br.readLine());
+
+		for (int i = 1; i <= T; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+			
+			sb.append(A + B).append('\n');
+		}
+		
+		System.out.println(sb);
+	}
+}
