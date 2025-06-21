065bc16f6ee37669fdb14b438feae1a1d4c78de9
woodongle <112540904+woodongle@users.noreply.github.com>
2025-06-21 14:27:01 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 176 ms, Memory: 19072 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
@@ -3,44 +3,50 @@ import java.io.*;
 
 class Main {
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		HashMap<String, String> map = new HashMap<>();
-		map.put(")", "(");
-		map.put("]", "[");
-		
-		while (true) {
-			Deque<String> stack = new ArrayDeque<>();
-			
-			String[] split = br.readLine().split("");
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-			if (split[0].equals(".")) {
-				return;
-			}
-			
-			for (int i = 0; i < split.length; i++) {
-				String str = split[i];
-				
-				if (str.equals("(") ||
-					str.equals(")") ||
-					str.equals("[") ||
-					str.equals("]")) {
-					
-					if (!stack.isEmpty() && stack.peekLast().equals(map.get(str))) {
-						stack.removeLast();
-						continue;
-					}
-					
-					stack.addLast(str);
-				}
-			}
-			
-			if (stack.isEmpty()) {
-				System.out.println("yes");
-			} else {
-				System.out.println("no");
-			}
-		}
-	}
+        while (true) {
+            String line = br.readLine();
+
+            if (line.equals(".")) {
+                break;
+            }
+
+            sb.append(solve(line)).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+
+    static String solve(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (c == '[' || c == '(') {
+                stack.push(c);
+            } else if (c == ']') {
+                if (stack.isEmpty() || stack.peek() != '[') {
+                    return "no";
+                }
+
+                stack.pop();
+            } else if (c == ')') {
+                if (stack.isEmpty() || stack.peek() != '(') {
+                    return "no";
+                }
+
+                stack.pop();
+            }
+        }
+
+        if (stack.isEmpty()) {
+            return "yes";
+        } else {
+            return "no";
+        }
+    }
 }
