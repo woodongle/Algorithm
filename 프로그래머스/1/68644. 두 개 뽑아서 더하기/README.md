a89a2d875a72e4d1f414be966e39d20e79f264aa
woodongle <112540904+woodongle@users.noreply.github.com>
2025-11-17 20:48:14 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 4.74 ms, Memory: 81.3 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/README.md
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 두 개 뽑아서 더하기 - 68644 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68644?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68644) 
 
 ### 성능 요약
 
-메모리: 85.7 MB, 시간: 2.45 ms
+메모리: 81.3 MB, 시간: 4.74 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 12일 20:19:18
+2025년 11월 17일 20:48:09
 
 ### 문제 설명
 
--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
@@ -1,22 +1,18 @@
 import java.util.*;
+import java.io.*;
 
 class Solution {
     public int[] solution(int[] numbers) {
-        int[] answer = {};
-        
-        TreeSet<Integer> set = new TreeSet<>();
+        TreeSet<Integer> treeSet = new TreeSet<>();
         for (int i = 0; i < numbers.length - 1; i++) {
             for (int j = i + 1; j < numbers.length; j++) {
-                set.add(numbers[i] + numbers[j]);
+                treeSet.add(numbers[i] + numbers[j]);
             }
         }
         
-        answer = new int[set.size()];
-        
-        for (int i = 0; i < answer.length; i++) {
-            answer[i] = set.pollFirst();
-        }
-        
+        int[] answer = treeSet.stream()
+            .mapToInt(Integer::intValue)
+            .toArray();
         return answer;
     }
 }
