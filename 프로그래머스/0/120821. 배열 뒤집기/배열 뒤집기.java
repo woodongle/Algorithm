c2748ecea2bcfa701a64c18e11808d52b072c0d5
woodongle <112540904+woodongle@users.noreply.github.com>
2025-11-24 20:37:16 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.02 ms, Memory: 93.9 MB -BaekjoonHub

--- 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.java
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.java
@@ -2,14 +2,8 @@ class Solution {
     public int[] solution(int[] num_list) {
         int[] reverseList = new int[num_list.length];
         
-        int reverseIndex = num_list.length - 1;
-        for (int num : num_list) {
-            if (reverseIndex < 0) {
-                break;
-            }
-            
-            reverseList[reverseIndex] = num;
-            reverseIndex--;
+        for (int i = 0; i < num_list.length; i++) {
+            reverseList[i] = num_list[num_list.length - 1 - i];
         }
         
         return reverseList;
