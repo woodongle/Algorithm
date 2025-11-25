7db79c714c5bbb6f05c4bf64bf915c696560489e
woodongle <112540904+woodongle@users.noreply.github.com>
2025-11-25 22:06:22 +0900

    [level 2] Title: 괄호 회전하기, Time: 51.84 ms, Memory: 85.5 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -1,42 +1,38 @@
 import java.util.*;
-import java.io.*;
 
 class Solution {
-    
-    private static final Map<Character, Character> map = new HashMap<>();
-    
-    public static int solution(String s) {
-        int count = 0;
+    public int solution(String s) {
+        Map<Character, Character> map = new HashMap<>();
         map.put(')', '(');
         map.put('}', '{');
         map.put(']', '[');
-
-        for (int i = 0; i < s.length(); i++) {
-            String str = s.substring(i) + s.substring(0, i);
+        
+        int sLength = s.length();
+        s += s;
+        int answer = 0;
+        
+        A:for (int i = 0; i < sLength; i++) {
+            Stack<Character> stack = new Stack<>();
             
-            if (isValid(str)) {
-                count++;
-            }
-        }
-
-        return count;
-    }
-
-    static boolean isValid(String str) {
-        Stack<Character> stack = new Stack<>();
-        
-        for (char ch : str.toCharArray()) {
-            if (map.containsKey(ch)) {
-                if (stack.isEmpty() || stack.peek() != map.get(ch)) {
-                    return false;
-                }
-                
-                stack.pop();
-            } else {
-                stack.push(ch);
+            for (int j = i; j < sLength + i; j++) {
+               char c = s.charAt(j);
+               
+               if (!map.containsKey(c)) {
+                   stack.push(c);
+               } else {
+                   if (!stack.isEmpty() && stack.peek() == map.get(c)) {
+                       stack.pop();
+                   } else {
+                       continue A;
+                   }
+               }
+           }
+            
+            if (stack.isEmpty()) {
+                answer++;
             }
         }
         
-        return stack.isEmpty();
+        return answer;
     }
 }
