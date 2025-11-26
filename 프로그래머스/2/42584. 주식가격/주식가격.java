560f213ab42ce8867b3eb5f7ef704115958b8e06
woodongle <112540904+woodongle@users.noreply.github.com>
2025-11-26 21:01:19 +0900

    [level 2] Title: 주식가격, Time: 18.73 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,10 +1,8 @@
-import java.util.*;
-
 class Solution {
     public int[] solution(int[] prices) {
         int[] answer = new int[prices.length];
         
-        for (int i = 0; i < prices.length; i++) {
+        for (int i = 0; i < prices.length - 1; i++) {
             for (int j = i + 1; j < prices.length; j++) {
                 answer[i]++;
                 
