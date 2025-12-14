1bccb46c9af0de7d0486aa5542a44a1db6ae885b
woodongle <112540904+woodongle@users.noreply.github.com>
2025-12-14 16:34:43 +0900

    [level 2] Title: 기능개발, Time: 2.37 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        ArrayDeque<Integer> deque = new ArrayDeque<>();
+        List<Integer> list = new ArrayList<>();
         int[] daysLeft = new int[progresses.length];
         for (int i = 0; i < daysLeft.length; i++) {
             daysLeft[i] = (int) Math.ceil((100.00 - progresses[i]) / speeds[i]);
@@ -15,14 +15,14 @@ class Solution {
             if (daysLeft[i] <= maxDay) {
                 count++;
             } else {
-                deque.addLast(count);
+                list.add(count);
                 count = 1;
                 maxDay = daysLeft[i];
             }
         }
         
-        deque.addLast(count);
-        return deque.stream()
+        list.add(count);
+        return list.stream()
             .mapToInt(Integer::intValue)
             .toArray();
     }
