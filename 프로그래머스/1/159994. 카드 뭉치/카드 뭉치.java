bdf6a5e55fc46068251ff0be55cf5b27e7236f7a
woodongle <112540904+woodongle@users.noreply.github.com>
2025-12-14 17:01:09 +0900

    [level 1] Title: 카드 뭉치, Time: 0.09 ms, Memory: 81.3 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -2,17 +2,29 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] cards1, String[] cards2, String[] goal) {
-        ArrayDeque<String> deck1 = new ArrayDeque(Arrays.asList(cards1));
-        ArrayDeque<String> deck2 = new ArrayDeque(Arrays.asList(cards2));
+        ArrayDeque<String> deque1 = new ArrayDeque<>();
+        ArrayDeque<String> deque2 = new ArrayDeque<>();
         
-        for (String target : goal) {
-            if (!deck1.isEmpty() && deck1.peek().equals(target)) {
-                deck1.poll();
-            } else if (!deck2.isEmpty() && deck2.peek().equals(target)) {
-                deck2.poll();
-            } else {
-                return "No";
+        for (String s : cards1) {
+            deque1.addLast(s);
+        }
+        
+        for (String s : cards2) {
+            deque2.addLast(s);
+        }
+        
+        for (String word : goal) {
+            if (word.equals(deque1.peek())) {
+                deque1.pollFirst();
+                continue;
             }
+            
+            if (word.equals(deque2.peek())) {
+                deque2.pollFirst();
+                continue;
+            }
+            
+            return "No";
         }
         
         return "Yes";
