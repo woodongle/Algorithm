780b26e495664d5cfe7f6ac05e68fa25a0a3edf9
woodongle <112540904+woodongle@users.noreply.github.com>
2025-12-17 20:17:14 +0900

    [level 2] Title: 할인 행사, Time: 87.62 ms, Memory: 115 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -2,31 +2,26 @@ import java.util.*;
 
 class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
-        Map<String, Integer> map = new HashMap<>();
+        Map<String, Integer> wantMap = new HashMap<>();
+        for (int i = 0; i < want.length; i++) {
+            wantMap.put(want[i], number[i]);
+        }
         
+        Map<String, Integer> resultMap = new HashMap<>();
         int count = 0;
-        A:for (int i = 0; i <= discount.length - 10; i++) {
-            for (int m = 0; m < want.length; m++) {
-                map.put(want[m], number[m]);
-            }
-            
+        for (int i = 0; i <= discount.length - 10; i++) {
             for (int j = i; j < i + 10; j++) {
-                String goods = discount[j];
+                String discountGoods = discount[j];
                 
-                if (!map.containsKey(goods)) {
-                    continue;
-                }
-                
-                map.put(goods, map.get(goods) - 1);
+                resultMap.put(discountGoods, 
+                              resultMap.getOrDefault(discountGoods, 0) + 1);
             }
             
-            for (String key : map.keySet()) {
-                if (map.get(key) > 0) {
-                    continue A;
-                }
+            if (resultMap.equals(wantMap)) {
+                count++;
             }
             
-            count++;
+            resultMap.clear();
         }
         
         return count;
