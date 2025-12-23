2e844bc48f49b5791a7897fecc9e35a23d391c2a
woodongle <112540904+woodongle@users.noreply.github.com>
2025-12-23 11:55:15 +0900

    [level 1] Title: 신고 결과 받기, Time: 136.93 ms, Memory: 160 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -2,50 +2,34 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
-        Map<String, Set<String>> reportMap = new HashMap<>();
+        Map<String, Integer> userIndexMap = new HashMap<>();
+        Map<String, Set<String>> reportedMap = new HashMap<>();
+        
+        for (int i = 0; i < id_list.length; i++) {
+            String reportedUserId = id_list[i];
+            userIndexMap.put(reportedUserId, i);
+            reportedMap.put(reportedUserId, new HashSet<>());
+        }
+        
         for (String s : report) {
             String[] split = s.split(" ");
             String reportUserId = split[0];
             String reportedUserId = split[1];
             
-            if (!reportMap.containsKey(reportUserId)) {
-                reportMap.put(reportUserId, new HashSet<>());
-            }
-            reportMap.get(reportUserId).add(reportedUserId);
-        }
-        
-        Map<String, Integer> totalReportedCountMap = new HashMap<>();
-        for (String reportUserId : reportMap.keySet()) {
-            Set<String> reportedUserIdSet = reportMap.get(reportUserId);
-            
-            for (String reportedUserId : reportedUserIdSet) {
-                totalReportedCountMap.put(reportedUserId, 
-                                         totalReportedCountMap.getOrDefault(reportedUserId, 0) + 1);
-            }
-        }
-        
-        List<String> reportedUserIds = new ArrayList<>();
-        for (String reportedUserId : totalReportedCountMap.keySet()) {
-            if (totalReportedCountMap.get(reportedUserId) >= k) {
-                reportedUserIds.add(reportedUserId);
-            }
-        }
-        
-        Map<String, Integer> userMap = new HashMap<>();
-        for (int i = 0; i < id_list.length; i++) {
-            userMap.put(id_list[i], i);
+            reportedMap.get(reportedUserId).add(reportUserId);
         }
         
         int[] answer = new int[id_list.length];
-        for (String reportUserId : reportMap.keySet()) {
-            Set<String> reportedUserIdSet = reportMap.get(reportUserId);
+        for (String reportedUserId : id_list) {
+            Set<String> reportUserIds = reportedMap.get(reportedUserId);
             
-            for (String reportedUserId : reportedUserIds) {
-                int index = userMap.get(reportUserId);
-                
-                if (reportedUserIdSet.contains(reportedUserId)) {
-                    answer[index]++;
-                }
+            if (reportUserIds.size() < k) {
+                continue;
+            }
+            
+            for (String reportUserId : reportUserIds) {
+                int index = userIndexMap.get(reportUserId);
+                answer[index]++;
             }
         }
         
