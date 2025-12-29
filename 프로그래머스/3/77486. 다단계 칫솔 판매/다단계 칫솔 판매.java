a654713060ae86e9b07e627912d4da64171131ae
woodongle <112540904+woodongle@users.noreply.github.com>
2025-12-29 20:06:07 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 78.55 ms, Memory: 149 MB -BaekjoonHub

--- 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
@@ -3,32 +3,32 @@ import java.util.*;
 class Solution {
     public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
         Map<String, String> referralMap = new HashMap<>();
-        Map<String, int[]> moneyMap = new HashMap<>();
-        
+        Map<String, Integer> moneyMap = new HashMap<>();
+
         for (int i = 0; i < enroll.length; i++) {
             referralMap.put(enroll[i], referral[i]);
-            moneyMap.put(enroll[i], new int[]{i, 0});
+            moneyMap.put(enroll[i], 0);
         }
-        
-        
+
+
         for (int i = 0; i < seller.length; i++) {
             String currentSeller = seller[i];
             int profit = amount[i] * 100;
-            
+
             while (!currentSeller.equals("-") && profit > 0) {
                 int referralMoney = profit / 10;
                 int mineMoney = profit - referralMoney;
-                
-                moneyMap.get(currentSeller)[1] += mineMoney;
-                
+
+                moneyMap.put(currentSeller, moneyMap.get(currentSeller) + mineMoney);
+
                 currentSeller = referralMap.get(currentSeller);
                 profit = referralMoney;
             }
         }
-        
+
         int[] answer = new int[enroll.length];
         for (int i = 0; i < answer.length; i++) {
-            answer[i] = moneyMap.get(enroll[i])[1];
+            answer[i] = moneyMap.get(enroll[i]);
         }
 
         return answer;
