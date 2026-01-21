e2c3af3687e0cb224e79380b5732d5b11815cb07
woodongle <112540904+woodongle@users.noreply.github.com>
2026-01-21 14:08:44 +0900

    [level 1] Title: 폰켓몬, Time: 10.07 ms, Memory: 92.6 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -2,12 +2,6 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] nums) {
-        int N = nums.length / 2;
-        Set<Integer> set = new HashSet<>();
-        for (int num : nums) {
-            set.add(num);
-        }
-        
-        return N > set.size() ? set.size() : N;
+        return Math.min((int) Arrays.stream(nums).distinct().count(), nums.length / 2);
     }
 }
